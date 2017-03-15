1a63be0917244ab58288d47a86fa58d9fd1b0e46
Tony <tony@gmail.com>
2017-03-15 20:46:53 +0800

    加入忘记的时间

--- app/src/main/java/com/weixinlite/android/Friends.java
+++ app/src/main/java/com/weixinlite/android/Friends.java
@@ -12,6 +12,7 @@ public class Friends extends DataSupport {
     private String name;
     private int imageId;
     private String msg;
+    private String time;
 
     public int getId() {
         return id;
@@ -43,5 +44,13 @@ public class Friends extends DataSupport {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
