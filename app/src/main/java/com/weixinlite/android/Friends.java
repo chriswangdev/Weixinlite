58d69f13029fce2fd2ac06fcf29e09f8a760f23d
Tony <tony@gmail.com>
2017-03-15 20:43:40 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/weixinlite/android/Friends.java
@@ -0,0 +1,47 @@
+package com.weixinlite.android;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by a on 2017/3/15 0015.
+ */
+
+public class Friends extends DataSupport {
+
+    private int id;
+    private String name;
+    private int imageId;
+    private String msg;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/weixinlite/android/MyListItem.java
@@ -0,0 +1,17 @@
+package com.weixinlite.android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by a on 2017/3/15 0015.
+ * by yuandiandev
+ * 用来显示每个单条的内容
+ */
+
+public class MyListItem extends LinearLayout {
+    public MyListItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
