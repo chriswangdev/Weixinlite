570253174eca273dc20763ff7e9160de2e20fd8f
Tony <tony@gmail.com>
2017-04-05 10:36:53 +0800

    增加通讯录进入聊天功能

---
+++ app/src/main/java/com/weixinlite/android/XiangxiActivity.java
@@ -0,0 +1,88 @@
+package com.weixinlite.android;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by a on 2017/4/5 0005.
+ */
+
+public class XiangxiActivity extends AppCompatActivity {
+
+    private static final String TAG = "XiangxiActivity";
+
+    private Friends friends;
+    private ImageView imageView;
+    private TextView textView_id;
+    private TextView textView_nicheng;
+
+    private Button btnSendMsg;
+    private Button btnChatVideo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xiangxi);
+        initToolBar();
+        initLayout();
+
+        btnSendMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(XiangxiActivity.this,ChattingActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("friends",friends);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+            }
+        });
+
+        btnChatVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(XiangxiActivity.this, "视频聊天", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initToolBar() {
+
+        friends = (Friends) getIntent().getSerializableExtra("friends");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        TextView textView = (TextView) findViewById(R.id.tool_bar_text);
+        textView.setText("详细信息");
+        toolbar.setTitle("");
+        toolbar.setNavigationIcon(R.mipmap.backup);
+        setSupportActionBar(toolbar);
+
+    }
+
+    private void initLayout() {
+        imageView = (ImageView) findViewById(R.id.item_img);
+        textView_id = (TextView) findViewById(R.id.item_name);
+        textView_nicheng = (TextView) findViewById(R.id.item_msg);
+
+        btnSendMsg = (Button) findViewById(R.id.send_msg);
+        btnChatVideo = (Button) findViewById(R.id.chat_with_vedio);
+
+
+        Glide.with(this).load(friends.getImageId()).into(imageView);
+        textView_id.setText(friends.getName());
+        textView_nicheng.setText("昵称：" + friends.getName());
+
+    }
+
+}
--- app/src/main/java/com/weixinlite/android/fragments/Fragment_tongxunlu.java
+++ app/src/main/java/com/weixinlite/android/fragments/Fragment_tongxunlu.java
@@ -1,5 +1,6 @@
 package com.weixinlite.android.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,12 +8,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.weixinlite.android.Friends;
 import com.weixinlite.android.Msg;
 import com.weixinlite.android.R;
+import com.weixinlite.android.XiangxiActivity;
 import com.weixinlite.android.util.Gettime;
 import com.weixinlite.android.util.Utility;
 
@@ -34,6 +37,8 @@ public class Fragment_tongxunlu extends Fragment {
     private ListView listView;
     private Button btn_save;
     private List<Friends> friendsList = new ArrayList<>();
+    private List<Friends> list = new ArrayList<>();
+    private List<Msg> msgList = new ArrayList<>();
     private TxlAdapter txlAdapter;
 
     public static Fragment_tongxunlu newInstance() {
@@ -46,13 +51,13 @@ public class Fragment_tongxunlu extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tongxunlu, container, false);
 
         listView = (ListView) view.findViewById(R.id.list_tongxunlu);
         //btn_save = (Button) view.findViewById(R.id.savetolitepal);
-        Utility.myImageId = Utility.getImageId("imgmy",getContext());
+        Utility.myImageId = Utility.getImageId("imgmy", getContext());
         findfriends();
         Log.e(TAG, "onCreateView: -----findfriends();------ size = " + friendsList.size());
         if (friendsList.size() == 0) {
@@ -63,23 +68,49 @@ public class Fragment_tongxunlu extends Fragment {
         txlAdapter = new TxlAdapter(getContext(), friendsList);
         listView.setAdapter(txlAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), XiangxiActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("friends", friendsList.get(position));
+                intent.putExtras(bundle);
+                getActivity().startActivity(intent);
+            }
+        });
+
 
         return view;
     }
 
     private void findfriends() {
-        friendsList = DataSupport.findAll(Friends.class);
+        list = DataSupport.findAll(Friends.class);
+
+        for (int i = 0; i < list.size(); i++) {
+            Friends friends = new Friends();
+            friends.setName(list.get(i).getName());
+            msgList = DataSupport.where("friendName = ?", friends.getName()).find(Msg.class);
+
+            Msg msg = new Msg();
+            friends.setIstop(list.get(i).getIstop());
+            friends.setImageId(list.get(i).getImageId());
+            friends.setMsgList(msgList);
+            this.friendsList.add(friends);
+
+        }
+
     }
 
     private void writetolitepal() {
 
         Friends friends = new Friends();
 
-        friends.setImageId(Utility.getImageId("beautyone",getContext()));//friends.setImageId(R.drawable.beautyone);
+        friends.setImageId(Utility.getImageId("beautyone", getContext()));//friends.setImageId(R
+        // .drawable.beautyone);
         friends.setName("小王");
 
         List<Msg> msgList = new ArrayList<>();
-        Msg msg = new Msg("你好",Msg.TYPE_RECEIVED);
+        Msg msg = new Msg("你好", Msg.TYPE_RECEIVED);
         msg.setFriendName(friends.getName());
         msg.setMsgtime(Gettime.getNowTime());
         msg.save();
@@ -94,7 +125,7 @@ public class Fragment_tongxunlu extends Fragment {
         //for (int i = 0; i < 2; i++) {
 
         friends = new Friends();
-        friends.setImageId(Utility.getImageId("beautytwo",getContext()));
+        friends.setImageId(Utility.getImageId("beautytwo", getContext()));
         friends.setName("小丽");
         //friends.setMsg("你好");
         //friends.setTime("10:20");
@@ -104,7 +135,7 @@ public class Fragment_tongxunlu extends Fragment {
 
 
         friends = new Friends();
-        friends.setImageId(Utility.getImageId("beautythree",getContext()));
+        friends.setImageId(Utility.getImageId("beautythree", getContext()));
         friends.setName("小小");
         //friends.setMsg("阿什顿飞");
         //friends.setTime("15:45");
@@ -114,7 +145,7 @@ public class Fragment_tongxunlu extends Fragment {
 
 
         friends = new Friends();
-        friends.setImageId(Utility.getImageId("beautyfour",getContext()));
+        friends.setImageId(Utility.getImageId("beautyfour", getContext()));
         friends.setName("妹子");
         //friends.setMsg("隧道股份");
         //friends.setTime("17:05");
@@ -125,7 +156,6 @@ public class Fragment_tongxunlu extends Fragment {
         // }
 
     }
-
 
 
 }
